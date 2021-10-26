dc4ed7b661e64c1d9266e833ac0f0b6c0523d464
Balaji <86100927+rba1aji@users.noreply.github.com>
2021-10-27 01:53:24 +0530

    Update main.cpp
    updated

--- main.cpp
+++ main.cpp
@@ -9,16 +9,21 @@ using namespace std;
 /	 			CLASS
 ***********************************/
 class Company{
-	string vacany_details;
 	public:
 	bool login();
 	void reg();
-	void Vacancy();
 };
 class Applicant{
 	public:
 	bool login();
 	void reg();
+};
+//Multiple Inheritance
+class Vacancy: public Company, public Applicant{
+	string vacancyDetails;
+	public:
+	void postVacancy();
+	void viewVacancy();
 };
 
 /***********************************
@@ -56,11 +61,6 @@ class Applicant{
 		cout<<"Company Registration successfull:)"<<endl;
 	}/**** C Register  ****/
 	
-	void Company::Vacancy(){
-		cout<<"Enter Vacany Details:\n"
-		cin>>vacany_details;
-	}
-	
 /***********************************
 /		APPLICANT CLASS METHODS
 ***********************************/
@@ -97,12 +97,73 @@ class Applicant{
 	}/**** A Register  ****/	
 	
 /***********************************
+/		VACANCY D-CLASS METHODS
+***********************************/
+
+	void Vacancy::postVacancy(){
+		cout<<"Enter job vacancy details:\n\t";
+		cin>>vacancyDetails;
+	}
+	void Vacancy::viewVacancy(){
+		cout<<"No vacancies available at this time.. Check back Later..\n";
+	}
+
+/***********************************
+/	   USER DIFINED FUNCTIONS
+***********************************/
+
+	void proceedA(){
+			Vacancy V;
+			cout<<"\n1.View Job Vacancies\n2.View Applicant Info\n3.Update Applicant Info\n";
+			int c2; cin>>c2;
+			switch(c2){
+				case 1:
+				V.viewVacancy();
+				break;
+				
+				case 2:
+				break;
+				
+				case 3:
+				break;
+				
+				default:
+				cout<<"Invalid Key:(\n";
+				proceedA();
+				break;
+			}
+	}/**** Proceed A ****/
+	
+		void proceedC(){
+			Vacancy V;
+			cout<<"\n1.Post Job Vacancies\n2.View Company Info\n3.Update Company Info\n";
+			int c3; cin>>c3;
+			switch(c3){
+				case 1:
+				V.postVacancy();
+				break;
+				
+				case 2:
+				break;
+				
+				case 3:
+				break;
+				
+				default:
+				cout<<"Invalid Key:(\n";
+				proceedC();
+				break;
+			}
+	}/**** Proceed C ****/
+	
+/***********************************
 /		 	MAIN FUNCTION
 ***********************************/
 	
 int main(){
 	Applicant A;
 	Company C;
+	Vacancy V;
 	
 	bool tmp1,tmp2;
 	
@@ -110,32 +171,34 @@ int main(){
 	
 	int c1; cin>>c1;
 	switch(c1){
-		case 1:
+/*1*/ case 1:
 		tmp1=A.login();
 		if(tmp1==false){
 			cout<<"\n";
 			main();
 		}
 		else if(tmp1==true){
-			break;
+			proceedA();
 		}
+		break;
 		
-		case 2:
+/*2*/ case 2:
 		A.reg();
 		main();
 		break;
 		
-		case 3:
+/*3*/ case 3:
 		tmp2=C.login();
 		if(tmp2==false){
 			cout<<"\n";
 			main();
 		}
 		else if(tmp2==true){
-			break;
+			proceedC();
 		}
+		break;
 		
-		case 4:
+/*4*/ case 4:
 		C.reg();
 		main();
 		break;
@@ -147,6 +210,6 @@ int main(){
 	}
 	return 0;
 }
-/**********************************
+/***********************************
 /				THE END
-**********************************/
+***********************************/
